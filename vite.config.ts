f32c3635b7999a68798054caaf0c16a8cf77251a
sarah <sarah.buisson@gmail.com>
2025-09-01 14:58:02 +0200

    various fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/three-ts-boilerplate/',
+  base: '/three-js-aquarium-challenge/',
   plugins: [react()],
 })
 // https://vitejs.dev/config/
